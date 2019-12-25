115b068d108a8073e09081e6826206961d903713
Kyle Thompson <kyle.thompson228@gmail.com>
2019-12-24 19:10:35 -0500

    use ttf font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Gohu GohuFont:pixelsize=11";
+static char *font = "GohuFont:pixelsize=11";
 static int borderpx = 2;
 
 /*
